c2dd518ba0972ecc0bb1820993e1f844dd8105d7
Jared Hall Garst <jared.h.garst@gmail.com>
2018-05-28 16:14:31 -0700

    environment: use pam_environment

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,8 @@
 # default XDG variables
 
-set -gx XDG_DATA_HOME $HOME/local/share
-set -gx XDG_CONFIG_HOME $HOME/config
-set -gx XDG_DATA_DIRS /usr/local/share/:/usr/share
-set -gx XDG_CONFIG_DIRS /etc/xdg
-set -gx XDG_CACHE_HOME $HOME/cache
-
 # personal scripts
 set -gx PATH $XDG_CONFIG_HOME/bin $PATH
 
-set -x PYTHONSTARTUP $XDG_CONFIG_HOME/python/python-startup.py
-set -x IPYTHONDIR $XDG_CONFIG_HOME/ipython
-set -x PASSWORD_STORE_DIR $HOME/pass
-
-set -x EDITOR "vim"
 set -gx VIMINIT 'let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
 
 # path to virsh machine
@@ -26,7 +15,6 @@ alias ctags "ctags --options=$XDG_CONFIG_HOME/ctags"
 alias tmux "tmux -f $XDG_CONFIG_HOME/tmux.conf"
 
 # go path
-set -x GOPATH $HOME/go
 set -gx PATH $GOPATH/bin $PATH
 
 function neomutt
