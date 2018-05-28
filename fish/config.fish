2c641b897570e38a6e3d65c9ba1e8561b873aaac
Jared Hall Garst <jared.h.garst@gmail.com>
2018-05-27 19:07:54 -0700

    xdg vars: folders are not hidden

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,10 @@
 # default XDG variables
 
-set -gx XDG_DATA_HOME $HOME/.local/share
-set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_DATA_HOME $HOME/local/share
+set -gx XDG_CONFIG_HOME $HOME/config
 set -gx XDG_DATA_DIRS /usr/local/share/:/usr/share
 set -gx XDG_CONFIG_DIRS /etc/xdg
-set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_CACHE_HOME $HOME/cache
 
 # personal scripts
 set -gx PATH $XDG_CONFIG_HOME/bin $PATH
