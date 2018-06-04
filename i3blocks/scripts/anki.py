fd1518c1c06c6526573642d6a216f0b0ebcb114e
Jared Hall Garst <jared.h.garst@gmail.com>
2018-06-04 11:25:55 -0700

    i3blocks: use local time for anki

--- i3blocks/scripts/anki.py
+++ i3blocks/scripts/anki.py
@@ -2,17 +2,32 @@
 '''Print number of cards due.'''
 
 from pathlib import Path
+import sqlite3
+import os
 
-DB = Path('$XDG_DATA_HOME', 'Anki2', 'User 1', 'collection.anki2')
+DB = Path(os.environ['XDG_DATA_HOME'], 'Anki2', 'User 1', 'collection.anki2')
 
+# type 0 is new cards
+# type 1 is learning cards
+# type 2 is repeated cards
 # due date is relative to collection creation date
-# this sql snippet gets the offset of cards due today
-'''
+DUE_QUERY = '''
 WITH today(offset) AS (
-    SELECT julianday(date('now')) - julianday(date(crt, 'unixepoch')) FROM col
+    SELECT
+        julianday(date('now', 'localtime'))
+        - julianday(date(crt, 'unixepoch', 'localtime'))
+    FROM col
 )
 SELECT COUNT(*) FROM cards
-WHERE type == 2 AND due <= (SELECT offset FROM today);
+WHERE
+    type == 0
+    OR (type == 1 AND due <= strftime('%s', 'now'))
+    OR (type == 2 AND due <= (SELECT offset FROM today))
+;
 '''
 
-print(0)
+with sqlite3.connect(str(DB), timeout=10) as conn:
+    with conn as cur:
+        (DUE,) = cur.execute(DUE_QUERY).fetchone()
+
+print(f'{DUE:2d}')
