e1e8c0354e3e11b54c19791b471e70655aab706b
Jared Hall Garst <jared.h.garst@gmail.com>
2020-03-19 19:47:30 -0700

    [sway] pulse audio is used for volume control

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
-sudo pacman -S --needed --noconfirm fd shellcheck
+sudo pacman -S --needed --noconfirm fd shellcheck libpulse
 pikaur --noedit --nodiff --needed --noconfirm -S chruby ruby-install
