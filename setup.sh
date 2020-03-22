c239bf20a55196dab59c91ddef85b4fe39592b9c
Jared Hall Garst <jared.h.garst@gmail.com>
2020-03-22 13:39:43 -0700

    use doas for privilege escalation

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
-sudo pacman -S --needed --noconfirm fd shellcheck libpulse vim-gitgutter
+doas pacman -S --needed --noconfirm fd shellcheck libpulse vim-gitgutter
 pikaur --noedit --nodiff --needed --noconfirm -S chruby ruby-install
