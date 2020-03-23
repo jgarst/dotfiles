a2deea2ff39b7539a4c7960873e46e5f568b9b51
Jared Hall Garst <jared.h.garst@gmail.com>
2020-03-23 13:28:50 -0700

    [setup] installs `sd`

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
-doas pacman -S --needed --noconfirm fd shellcheck libpulse vim-gitgutter
+doas pacman -S --needed --noconfirm fd shellcheck libpulse vim-gitgutter sd
 pikaur --noedit --nodiff --needed --noconfirm -S chruby ruby-install
