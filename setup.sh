726e3c3cf3fa2773f1cb1bb013518133e6d95b4f
Jared Hall Garst <jared.h.garst@gmail.com>
2020-03-24 00:41:51 -0700

    [setup] installs bat

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env sh
 
-doas pacman -S --needed --noconfirm fd shellcheck libpulse vim-gitgutter sd
+doas pacman -S --needed --noconfirm fd shellcheck libpulse vim-gitgutter sd bat
 pikaur --noedit --nodiff --needed --noconfirm -S chruby-git ruby-install
+
+mandb
+
+bat cache --build
