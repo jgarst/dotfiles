e47d1206d39ca08d95145da257364524d49094b5
Jared Hall Garst <jared.h.garst@gmail.com>
2020-03-26 14:04:32 -0700

    [setup] installs pass-git
    unreleased pass commits are needed to support copying in wayland

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
 doas pacman -S --needed --noconfirm fd shellcheck libpulse vim-gitgutter sd bat
-pikaur --noedit --nodiff --needed --noconfirm -S chruby-git ruby-install zoom
+pikaur --noedit --nodiff --needed --noconfirm -S chruby-git ruby-install zoom pass-git
 
 mandb
 
