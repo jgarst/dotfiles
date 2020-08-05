2a4b09474d85912954934d115e6e602c6ec9f9d5
Jared Hall Garst <jhg@fastmail.com>
2020-08-04 21:06:16 -0700

    setup: describes ios mounting dependencies

--- config/setup.sh
+++ config/setup.sh
@@ -2,6 +2,7 @@
 
 packages=(
     base
+    base-devel
     linux
     linux-firmware
     neovim
@@ -19,11 +20,21 @@ packages=(
     pass
     brightnessctl
 )
+# rclone?
 
 sound=(
     alsa-utils
     pulseaudio
     pavucontrol
+)
+
+ios=(
+    libimobiledevice
+    ifuse
+)
+
+x11=(
+    feh
 )
 
 wayland=(
@@ -40,9 +51,12 @@ aurs=(
     dmenu-height
 )
 
-doas pacman -S --needed "${packages[@]}" "${sound[@]}"
+doas pacman -S --needed "${packages[@]}" "${sound[@]}" "${x11[@]}" "${ios[@]}"
 # pikaur --noedit --nodiff --needed -S "${aurs[@]}"
 
 mandb
 
 bat cache --build
+
+mkdir "${XDG_CONFIG_HOME}"/rclone
+pass rclone_conf > "${XDG_CONFIG_HOME}"/rclone
