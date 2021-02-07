a368af78319034a527c854527baa2d7fda6180a0
Jared Hall Garst <jhg@fastmail.com>
2021-02-06 21:22:04 -0500

    feature(cookiecutter): default python project
    Create a cookiecutter with all the traditional linters and configuration
    set up.

---
+++ config/cookiecutter/hooks/post_gen_project.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -xeEuo pipefail
+
+git init
+pyenv local 3.9.1
+pyenv exec python -m venv env --prompt {{cookiecutter.project}}
+pyenv local --unset
+
+env/bin/pip install pip-tools
+env/bin/pip-compile
+
+git add -A
+git commit -m "inital commit"
+
+env/bin/pip-sync
+
+env/bin/pre-commit install --hook-type commit-msg
